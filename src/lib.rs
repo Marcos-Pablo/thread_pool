5e60f214f8d279b4dbbb676e1dc94d08824a4c89
Marcos Pablo <marquin.pablo@gmail.com>
2026-05-29 16:56:22 -0300

    Simple http server with thread pool

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size parameter is the number of threads in the pool.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `PoolCreationError::ZeroSize` if the size is zero.
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError::ZeroSize);
+        }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            let worker = Worker::new(id, Arc::clone(&receiver));
+            workers.push(worker);
+        }
+
+        Ok(ThreadPool { workers, sender })
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {id} got a job: executing");
+
+                job();
+            }
+        });
+
+        Worker { id, thread }
+    }
+}
+
+#[derive(Debug)]
+pub enum PoolCreationError {
+    ZeroSize,
+}
+
+impl std::fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PoolCreationError::ZeroSize => write!(f, "Thread pool size must be greater than zero."),
+        }
+    }
+}
+
+impl std::error::Error for PoolCreationError {}
